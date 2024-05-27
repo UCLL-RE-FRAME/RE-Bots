3006b2a50ef623e0362ee241eb4d9c413781a1e8
Ofrenay <oscar.frenay@student.ucll.be>
2024-05-27 15:55:14 +0200

    Event/Project-tiles arragement (scrollable list)

--- Server/Components/link.js
+++ Server/Components/link.js
@@ -7,8 +7,9 @@ let template = document.createElement("template");
 template.innerHTML = /*html*/ `
   <style>
         img{
-        width:100%;
-        height: 200px;
+        width: 16em;
+        height: 16em;
+        border-radius: 12px;
     }
     /*
     :host { font-family: sans-serif; }  
@@ -23,37 +24,39 @@ template.innerHTML = /*html*/ `
 
 //#region CLASS
 window.customElements.define(
-  "link-ʤ",
-  class extends HTMLElement {
-    constructor() {
-      super();
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(template.content.cloneNode(true));
-      this.$img = this._shadowRoot.querySelector("img");
-    }
+	"link-ʤ",
+	class extends HTMLElement {
+		constructor() {
+			super();
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
+			this.$img = this._shadowRoot.querySelector("img");
+		}
 
-    connectedCallback() {
-      this.$img.src = `../images/${this.getAttribute("thumb")}.${this.getAttribute("format")}`;
-      // console.log(this.getAttribute("thumb"));
-      //   this.addEventListener("go", (e) => {
-      //     // console.log(e); // logs menu button actions
-      //     this.$main.setAttribute("showing", e.detail.id);
-      //   });
-      //   this.$button1.addEventListener("click", () => {
-      //     console.log("TEST  HALLO");
-      //   });
-    }
-    // static get observedAttributes() {
-    //   return ["thumb"];
-    // }
-    // attributeChangedCallback(name, oldValue, newValue) {
-    //   switch (name) {
-    //     case "thumb":
-    //       console.log(`[FLAG] ${oldValue} => ${newValue}`)
-    //       this.$img.src = `../images/${newValue}.jpg`|| `../images/${newValue}.png`;
-    //       break;
-    //   }
-    // }
-  }
+		connectedCallback() {
+			this.$img.src = `../images/${this.getAttribute(
+				"thumb"
+			)}.${this.getAttribute("format")}`;
+			// console.log(this.getAttribute("thumb"));
+			//   this.addEventListener("go", (e) => {
+			//     // console.log(e); // logs menu button actions
+			//     this.$main.setAttribute("showing", e.detail.id);
+			//   });
+			//   this.$button1.addEventListener("click", () => {
+			//     console.log("TEST  HALLO");
+			//   });
+		}
+		// static get observedAttributes() {
+		//   return ["thumb"];
+		// }
+		// attributeChangedCallback(name, oldValue, newValue) {
+		//   switch (name) {
+		//     case "thumb":
+		//       console.log(`[FLAG] ${oldValue} => ${newValue}`)
+		//       this.$img.src = `../images/${newValue}.jpg`|| `../images/${newValue}.png`;
+		//       break;
+		//   }
+		// }
+	}
 );
 //#endregion CLASS
