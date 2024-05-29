2383cfd07f25adebe612bcde562faedbbc175cde
Ofrenay <oscar.frenay@student.ucll.be>
2024-05-29 10:52:19 +0200

    Soundboard fix

--- Frontend/components/soundboard.js
+++ Frontend/components/soundboard.js
@@ -12,13 +12,13 @@ window.customElements.define(
 			this.div.id = "board";
 			this.div.innerHTML = `
           <div class="square color1" id="rocky-theme-tune-mp3cut"><a href="#" class="btn blue">rocky</a></div>
-          <div class="square color2" id="windows-xp-shutdown"> <a href="#" class="btn red">shutdown</a></div>
-          <div class="square color2" id="awkward-cricket-sound-effect"><a href="#" class="btn green">silence</a></div>
-          <div class="square color1" id="samson-jaah-1"> <a href="#" class="btn purple">samson</a></div>
-          <div class="square color1" id="okay-guy"> <a href="#" class="btn yellow">okay</a></div>
-          <div class="square color2" id="i-have-several-questions"><a href="#" class="btn orange">questions</a></div>
-          <div class="square color2" id="ringtone_20"><a href="#" class="btn navy">my name is jef</a></div>
-          <div class="square color1" id="tf_nemesis"> <a href="#" class="btn pink">nemesis</a></div>
+          <div class="square color2" id="windows-xp-shutdown"> <a href="#" class="btn blue">shutdown</a></div>
+          <div class="square color2" id="awkward-cricket-sound-effect"><a href="#" class="btn blue">silence</a></div>
+          <div class="square color1" id="samson-jaah-1"> <a href="#" class="btn blue">samson</a></div>
+          <div class="square color1" id="okay-guy"> <a href="#" class="btn blue">okay</a></div>
+          <div class="square color2" id="i-have-several-questions"><a href="#" class="btn blue">questions</a></div>
+          <div class="square color2" id="ringtone_20"><a href="#" class="btn blue">my name is jef</a></div>
+          <div class="square color1" id="tf_nemesis"> <a href="#" class="btn blue">nemesis</a></div>
     `;
 
 			this.style = document.createElement("style");
@@ -44,7 +44,7 @@ window.customElements.define(
             height: 100%;
           }
           .square {
-            height: 24%;
+            height: 25%;
             width: 50%;
           }
           .color1 {
@@ -62,9 +62,9 @@ window.customElements.define(
             color:white;
           }
           .btn {
-              border-radius: 5px;
+              border-radius: 30px;
               padding: 5px 10px;
-              font-size: 18px;
+              font-size: 24px;
               text-decoration: none;
               margin-left:10%;
               color: #fff;
@@ -84,76 +84,13 @@ window.customElements.define(
             }
             
             .blue {
-              background-color: #55acee;
-              box-shadow: 0px 5px 0px 0px #3C93D5;
+              background-color: #002757;
+              box-shadow: 0px 5px 0px 0px #002777;
             }
             
             .blue:hover {
-              background-color: #6FC6FF;
+              background-color: #002777;
             }
-            .red {
-              background-color: #e74c3c;
-              box-shadow: 0px 5px 0px 0px #CE3323;
-            }
-            
-            .red:hover {
-              background-color: #FF6656;
-            }
-            .green {
-              background-color: #2ecc71;
-              box-shadow: 0px 5px 0px 0px #15B358;
-            }
-            
-            .green:hover {
-              background-color: #48E68B;
-            }
-            .navy {
-              background-color: #e3b5ab;
-              
-              box-shadow: 0px 5px 0px 0px #caa198;
-            }
-            
-            .navy:hover {
-              background-color: #fdcabf;
-            }
-            .pink {
-              background-color: #b8afd5;
-              
-              box-shadow: 0px 5px 0px 0px #938caa;
-            }
-            
-            .pink:hover {
-              background-color: #ddd5ef;
-            }
-
-            
-
-              .purple {
-                  background-color: #9b59b6;
-                  box-shadow: 0px 5px 0px 0px #82409D;
-              }
-              
-              .purple:hover {
-                  background-color: #B573D0;
-              }
-              
-              .orange {
-                  background-color: #e67e22;
-                  box-shadow: 0px 5px 0px 0px #CD6509;
-              }
-              
-              .orange:hover {
-                  background-color: #FF983C;
-              }
-              
-              .yellow {
-                  background-color: #f1c40f;
-                  box-shadow: 0px 5px 0px 0px #D8AB00;
-              }
-              
-              .yellow:hover {
-                  background-color: #FFDE29;
-              }
   `;
 
 			this._shadowroot = this.attachShadow({mode: "open"});
